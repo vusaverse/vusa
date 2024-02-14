cc8e3565e929fe0b198304998dc60e2682a04fc5
tin900 <t.iwan@vu.nl>
2024-02-14 11:29:08 +0100

    change: use system variables for clear_global_proj

--- R/clear_objects.R
+++ R/clear_objects.R
@@ -1,25 +1,37 @@
-#' Clear global.
+#' Clears the global environment except for objects specified in the DEFAULT_KEEP_LIST system variable.
 #'
-#' Clear global environment except for project specific objects.
-#' @param ... which object(s) to keep.
+#' This function removes all objects from the global environment except for those specified
+#' in the DEFAULT_KEEP_LIST system variable. The variable is expected to be a space-separated
+#' list of object names. If the variable is not set, the function will not run.
 #'
+#' @note This function should be used with caution as it modifies the global environment.
+#'
+#' @seealso \code{\link[base]{rm}} for the underlying mechanism used to remove objects.
+#'
+#' @keywords internal
 #' @export
-clear_global <- function(...) {
-  dots <- match.call(expand.dots = FALSE)$...
-  if (length(dots) &&
-      !all(vapply(dots, function(x) is.symbol(x) || is.character(x), NA, USE.NAMES = FALSE))) {
-    stop("... must contain names or character strings")
+clear_global_proj <- function() {
+  # Retrieve the default keep list from the environment variable
+  env_default_keep_list <- Sys.getenv("DEFAULT_KEEP_LIST")
+  
+  # Check if the environment variable is set and convert it to a list of object names
+  if (!env_default_keep_list == "") {
+    default_keep_list <- strsplit(env_default_keep_list, " ")[[1]]
+  } else {
+    # Handle the case where the environment variable is not set
+    # For example, you could throw an error or use a fallback list
+    stop("The 'DEFAULT_KEEP_LIST' environment variable is not set.")
   }
-  names <- vapply(dots, as.character, "")
-  if (length(names) == 0L) names <- character()
-  list <- .Primitive("c")(list, names)
-  rm(
-    list = setdiff(
-      ls(envir = .GlobalEnv),
-      c("Documentatie", "Branch", "Network_directory", list)
-    ),
-    envir = .GlobalEnv
-  )
+  
+  # Get the names of all objects in the global environment
+  all_objects <- ls(envir = .GlobalEnv)
+  
+  # Determine which objects to remove by finding the difference between all objects and the default keep list
+  objects_to_remove <- setdiff(all_objects, default_keep_list)
+  
+  # Remove the objects from the global environment
+  # rm(list = objects_to_remove, envir = .GlobalEnv)
+  rm(list = objects_to_remove, envir = parent.frame())
 }
 
 #' clear script objects.
