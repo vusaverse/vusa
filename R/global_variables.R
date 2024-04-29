cbf5649cd3ceaff463d3d92d1783717d162851df
Tomer Iwan <t.iwan@vu.nl>
2024-04-29 14:55:40 +0200

    change: add global variables using utils

---
+++ R/global_variables.R
@@ -0,0 +1,11 @@
+utils::globalVariables(c(
+  ".data", "Afhankelijkheid", "Analysis_set_7", "Bestand", "Build_Analysis_set",
+  "Clear_script_objects", "Documentatie_add_csv", "Documentatie_remove", "Geslaagd",
+  "Message", "Variabele", "Veldnaam", "arrange", "capture.output", "check_installed_package",
+  "column_number", "determine_inputfiles", "duplicates_in_column", "end", "everything",
+  "filename", "filter", "get_values", "group_by", "length_of_string", "line",
+  "line_number", "linter", "max_length_of_object_name", "messages", "mutate_if",
+  "na.omit", "print_last_modified", "rename", "setNames", "some_of", "start", "str_c",
+  "str_detect", "str_detect_in_file", "strict_left_join", "summarise", "sym",
+  "tibble_row", "type", "ui_done", "unite"
+))
