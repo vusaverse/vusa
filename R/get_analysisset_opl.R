4b13c1efec170586481d97aee1566c8fb29747f0
Hajo <h.b.bons@vu.nl>
2025-02-03 15:11:15 +0100

    feat: added VAKAS and OPLAS get functions

---
+++ R/get_analysisset_opl.R
@@ -0,0 +1,24 @@
+#' Get the analysis set from the network drive
+#'
+#' @param columns List of specific columns to read in.
+#' @param AS_path Path of the location for the analysis set
+#' @return OPLAS analysis set
+#' @export
+get_analysisset_opl <- function(columns = NULL, AS_path = NULL) {
+  if (is.null(AS_path)) {
+    if (!any(Sys.getenv(c("OUTPUT_DIR", "OPLAS_PATH")) == "")) {
+      AS <- paste0(Sys.getenv("OUTPUT_DIR"), Sys.getenv("BRANCH"), "/", Sys.getenv("OPLAS_PATH"))
+    } else {
+      stop("system variables for as_path are missing")
+    }
+  }
+
+  if (is.null(columns)) {
+    returnvar <- readr::read_rds(AS)
+  } else {
+    returnvar <- readr::read_rds(AS) %>%
+      dplyr::select(columns)
+  }
+
+  returnvar
+}
